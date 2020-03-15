5169371447b19934d6bb125d7135cc561a87624f
Adam Byrd <ajbyrd0723@gmail.com>
2020-03-15 13:05:16 -0500

    models created and registration works

--- capstoneapp/models/customer.py
+++ capstoneapp/models/customer.py
@@ -6,25 +6,25 @@ from django.dispatch import receiver
 class Customer(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length = 25, null=True)
-    email = models.CharField(max_length = 40, null=True)
-    first_name = models.CharField(max_length = 25, null=True)
-    last_name = models.CharField(max_length = 25, null=True)
+    # username = models.CharField(max_length = 25, null=True)
+    # email = models.CharField(max_length = 40, null=True)
+    # first_name = models.CharField(max_length = 25, null=True)
+    # last_name = models.CharField(max_length = 25, null=True)
 
-    def __str__(self):
-        return f'{self.user.first_name} {self.user.last_name}'
+    # def __str__(self):
+    #     return f'{self.user.first_name} {self.user.last_name}'
 
 
-    # Every time a `User` is created, a matching `Librarian`
-    # object will be created and attached as a one-to-one
-    # property
-    @receiver(post_save, sender=User)
-    def create_customer(sender, instance, created, **kwargs):
-        if created:
-            Customer.objects.create(user=instance)
+    # # Every time a `User` is created, a matching `Librarian`
+    # # object will be created and attached as a one-to-one
+    # # property
+    # @receiver(post_save, sender=User)
+    # def create_customer(sender, instance, created, **kwargs):
+    #     if created:
+    #         Customer.objects.create(user=instance)
 
-    # Every time a `User` is saved, its matching `Librarian`
-    # object will be saved.
-    @receiver(post_save, sender=User)
-    def save_customer(sender, instance, **kwargs):
-        instance.customer.save()
+    # # Every time a `User` is saved, its matching `Librarian`
+    # # object will be saved.
+    # @receiver(post_save, sender=User)
+    # def save_customer(sender, instance, **kwargs):
+    #     instance.customer.save()
